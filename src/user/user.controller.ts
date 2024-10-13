fbedf124bffcad695eb63289f696ef76afbf83e7
Cengiz Gürtusgil <cengiz.guertusgil@gmail.com>
2024-10-13 03:03:00 +0200

    fix backend

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,6 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  UseGuards,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserRequestDto } from './dto/create-user-request.dto';
 import { UserService } from './user.service';
-import { NoFilesInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CurrentUser } from '../auth/current-user.decorator';
 import { User } from '@prisma/client';
@@ -18,7 +10,6 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
-  @UseInterceptors(NoFilesInterceptor())
   createUser(@Body() request: CreateUserRequestDto) {
     return this.userService.createUser(request);
   }
